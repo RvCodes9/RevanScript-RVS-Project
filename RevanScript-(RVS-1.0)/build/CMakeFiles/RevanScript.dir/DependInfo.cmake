422fa14f60c791cb06502d7011da6befef3ddcd6
RvCodes9 <rvanbabayev83@gmail.com>
2026-04-11 10:02:40 -0400

    Cmake Build

---
+++ RevanScript-(RVS-1.0)/build/CMakeFiles/RevanScript.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/revan/Documents/GitHub/RevanScript-RVS-Project/RevanScript-(RVS-1.0)/src/main.c" "CMakeFiles/RevanScript.dir/src/main.c.o" "gcc" "CMakeFiles/RevanScript.dir/src/main.c.o.d"
+  "/home/revan/Documents/GitHub/RevanScript-RVS-Project/RevanScript-(RVS-1.0)/src/rvsbuf.c" "CMakeFiles/RevanScript.dir/src/rvsbuf.c.o" "gcc" "CMakeFiles/RevanScript.dir/src/rvsbuf.c.o.d"
+  "/home/revan/Documents/GitHub/RevanScript-RVS-Project/RevanScript-(RVS-1.0)/src/rvsctl.c" "CMakeFiles/RevanScript.dir/src/rvsctl.c.o" "gcc" "CMakeFiles/RevanScript.dir/src/rvsctl.c.o.d"
+  "/home/revan/Documents/GitHub/RevanScript-RVS-Project/RevanScript-(RVS-1.0)/src/rvsflg.c" "CMakeFiles/RevanScript.dir/src/rvsflg.c.o" "gcc" "CMakeFiles/RevanScript.dir/src/rvsflg.c.o.d"
+  "/home/revan/Documents/GitHub/RevanScript-RVS-Project/RevanScript-(RVS-1.0)/src/rvsio.c" "CMakeFiles/RevanScript.dir/src/rvsio.c.o" "gcc" "CMakeFiles/RevanScript.dir/src/rvsio.c.o.d"
+  "/home/revan/Documents/GitHub/RevanScript-RVS-Project/RevanScript-(RVS-1.0)/src/rvsmem.c" "CMakeFiles/RevanScript.dir/src/rvsmem.c.o" "gcc" "CMakeFiles/RevanScript.dir/src/rvsmem.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ RevanScript-(RVS-1.0)/build/CMakeFiles/RevanScript.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RevanScript.dir/src/main.c.o"
+  "CMakeFiles/RevanScript.dir/src/main.c.o.d"
+  "CMakeFiles/RevanScript.dir/src/rvsbuf.c.o"
+  "CMakeFiles/RevanScript.dir/src/rvsbuf.c.o.d"
+  "CMakeFiles/RevanScript.dir/src/rvsctl.c.o"
+  "CMakeFiles/RevanScript.dir/src/rvsctl.c.o.d"
+  "CMakeFiles/RevanScript.dir/src/rvsflg.c.o"
+  "CMakeFiles/RevanScript.dir/src/rvsflg.c.o.d"
+  "CMakeFiles/RevanScript.dir/src/rvsio.c.o"
+  "CMakeFiles/RevanScript.dir/src/rvsio.c.o.d"
+  "CMakeFiles/RevanScript.dir/src/rvsmem.c.o"
+  "CMakeFiles/RevanScript.dir/src/rvsmem.c.o.d"
+  "RevanScript"
+  "RevanScript.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/RevanScript.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
